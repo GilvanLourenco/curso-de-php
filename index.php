fd072602f67ccaa30178f1ccffa8ed019df321a7
Gilvan Lourenço <gilvann.lourenco@gmail.com>
2023-08-30 20:31:42 -0300

    Inciando modulo dos tipos de dados

--- index.php
+++ index.php
@@ -16,13 +16,19 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Modulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Intefração CSS</a></li>
                         <li><a href="exercicio.php?dir=basico&file=comentario">Comentários PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
                     </ul>
                 </div>
             </nav>
---
+++ tipos/int.php
@@ -0,0 +1,17 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+var_dump(11);
+echo '<br>';
+
+// Constante que mostra o inteiro máximo suportado pelo php na máqina, depende da arquitetura do processador.
+// echo PHP_INT_MAX, '<br';
+// Constanto que mostra o menor inteiro suportado pelo php na máquina
+// echo PHP_INT_MIN;
+echo 017, '<br>'; //Base octal
+echo 0b11000111001, '<br>'; //Base binparia
+echo 0x1fac, '<br>'; //Base hexadecimal
+
+?>
