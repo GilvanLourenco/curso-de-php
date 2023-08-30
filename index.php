9e874de8e94f9e7eec56c08341d9bad679859335
Gilvan Lourenço <gilvann.lourenco@gmail.com>
2023-08-29 21:22:19 -0300

    Exemplo ola.php

--- index.php
+++ index.php
@@ -18,73 +18,7 @@
                 <div class="modulo verde">
                     <h3>Modulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo vermelho">
-                    <h3>Modulo 02</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo azul">
-                    <h3>Modulo 03</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo roxo">
-                    <h3>Modulo 04</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo laranja">
-                    <h3>Modulo 05</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo verde-escuro">
-                    <h3>Modulo 06</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo vermelho-escuro">
-                    <h3>Modulo 07</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo azul-escuro">
-                    <h3>Modulo 08</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-                <div class="modulo roxo-escuro">
-                    <h3>Modulo 09</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício D</a></li>
-                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                     </ul>
                 </div>
             </nav>
