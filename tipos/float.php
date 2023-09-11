c7d0809b3e1218ae914e9f751a632be22397f737
Gilvan Lourenço <gilvann.lourenco@gmail.com>
2023-09-11 20:31:59 -0300

    foat

--- tipos/float.php
+++ tipos/float.php
@@ -8,5 +8,5 @@ echo PHP_FLOAT_MIN, '<br>';
 //Adição de zero, ou seja 1.2x10 elevado a 3 adicio
 echo 1.2e3; // 1200
 echo '<br>';
-echo 13e-3; // 13*10 elevado a -3 = 0.013
+echo 13e-3; // 13*10 elevado a -3 = 0.013, indica quantas casas a virgula irá andar e3 positivo, e-3 negativo
 ?>
