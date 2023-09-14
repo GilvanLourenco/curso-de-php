ad6b587bfa612b8e603775ea1c2574f70e9e1ffc
Gilvan Lourenço <gilvann.lourenco@gmail.com>
2023-09-14 17:01:30 -0300

    strings

--- tipos/string.php
+++ tipos/string.php
@@ -1,1 +1,35 @@
 <div class="titulo">Strings</div>
+<?php
+    echo 'Eu sou uma string';
+    echo '<br>';
+    var_dump("Eu também");//o acento é contando como caractere
+    echo '<br>';
+
+    // Concatenação de strings
+    echo "Nós também". ' somos';
+    echo '<br>';
+
+    echo "O echo", " também aceita", " vigulas";
+    echo '<br>';
+
+    print("também existem a função print");
+    echo '<br>';
+
+    echo strtoupper("Maximizado");
+    echo '<br>';
+    echo strtolower("MINIMIZADO");
+    echo '<br>';
+    echo ucfirst("só a primeira letra");
+    echo '<br>';
+    echo ucwords('todas as palavras');
+    echo '<br>';
+
+    echo strlen("quantas letras");
+    echo '<br>';
+    echo strlen('Eu também');
+    echo '<br>';
+    echo mb_strlen('Eu também');
+    echo '<br>';
+    echo mb_strlen('Eu também', 'utf-8');
+
+?>
